849d975bbd2be7139fda4bd75d9738b6fa01ffa1
EricJ <ericjardon@hotmail.com>
2021-06-10 20:01:52 -0500

    Last fixes to register volunteer msg

--- src/Donate/Donate.jsx
+++ src/Donate/Donate.jsx
@@ -153,9 +153,6 @@ const Donate = () => {
                     <div class="flex flex-wrap justify-center sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
                         <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
                             <div class="w-20 h-20 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-5 flex-shrink-0">
-                                {/* <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
-                                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                                </svg> */}
                                 < TiGroup class="w-12 h-12 " />
                             </div>
                             <div class="flex-grow">
@@ -251,8 +248,8 @@ const Donate = () => {
                             onClick={submitVolunteer}>
                             Contáctanos
                         </button>
-                        <p class="text-xs text-gray-500 mt-3">
-                            {msg}
+                        <p class="text-xs text-gray-500 mt-3 text-center" style={{fontSize:18}}>
+                           {msg}
                         </p>
                     </div>
                 </div>
